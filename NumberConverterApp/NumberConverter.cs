7977191b92af196a4a7b26470243d9f7dd0fa9c7
fummon <72984490+fummon@users.noreply.github.com>
2023-01-12 16:03:28 -0300

    .

---
+++ NumberConverterApp/NumberConverter.cs
@@ -0,0 +1,41 @@
+﻿namespace NumberConverterApp;
+public class NumberConverter
+{
+    public string ConvertToNumeral(int number)
+    {
+        if (number < 0 || number > 2000)
+        {
+            return "Please enter a number between 1 - 2000";
+        }
+
+        SortedDictionary<int, string> numberToNumeral = new() {
+            {1, "I"},
+            {4, "IV"},
+            {5, "V"},
+            {9, "IX"},
+            {10, "X"},
+            {40, "XL"},
+            {50, "L"},
+            {90, "XC"},
+            {100, "C"},
+            {400, "CD"},
+            {500, "D"},
+            {900, "CM"},
+            {1000, "M"}
+        };
+
+        string romanNumerals = "";
+
+        foreach (int key in numberToNumeral.Keys.OrderByDescending(x => x))
+        {
+            while (number >= key)
+            {
+                number -= key;
+                romanNumerals += (numberToNumeral[key]);
+            }
+        }
+
+        return romanNumerals;
+    }
+}
+
---
+++ NumberConverterApp_Tests/NumberConverter_Tests.cs
@@ -0,0 +1,40 @@
+﻿using NumberConverterApp;
+
+namespace NumberConverterApp_Tests;
+
+[TestClass]
+public class NumberConverter_Tests
+{
+    private NumberConverter _converter;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _converter = new(); 
+    }
+
+    [DataRow(1999, "MCMXCIX")]
+    [DataRow(938, "CMXXXVIII")]
+    [DataRow(439, "CDXXXIX")]
+    [DataRow(92, "XCII")]
+    [DataRow(34, "XXXIV")]
+    [DataRow(12, "XII")]
+    [DataRow(4, "IV")]
+    [DataTestMethod]
+    public void ConvertToNumeral_GivenValidNumber_ConvertsToRomanNumeral(int number, string numeral)
+    {
+        string result = _converter.ConvertToNumeral(number);
+        result.Equals(numeral); 
+    }
+
+    [DataRow(2001)]
+    [DataRow(-1)]
+    [DataTestMethod]
+    public void ConvertToNumeral_InvalidNumber_ReturnsErrorMessage(int number)
+    {
+        string expected = "Please enter a number between 1 - 2000";
+
+        string result = _converter.ConvertToNumeral(number);
+        result.Equals(expected);
+    }
+}
